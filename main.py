d2c11b9db1d8c852c4f577eb3b56c22c958f68d2
Stegeran Darius <40896007+Rou404@users.noreply.github.com>
2022-02-28 17:28:19 +0200

    Update main.py
    Program no longer terminates before the operand stack is empty and added final result

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 # This is an optional project for Formal Languages and Automata Theory 2021-2022.
 # Done by Stegeran Darius Cosmin
-# You need to use python3.10 or higher in order to work since switch statements were added only in 3.10
 
-
-#Initialization of lists
-
-expression = [x for x in "b*(c-d)"]
-expression = expression[::-1]                           #List reversed 
-operator = []                                           #Operator Stack
-evaluation = []                                         #Evaluation stack
-operator_value = {"+": 1, "-": 1, "*": 2, "/": 2}       #Dictionary holding priority values for the oprands
+expression = [x for x in "(A+B)*C/E-F"]
+expression = expression[::-1]
+operator = []
+evaluation = []
+operator_value = {"+": 1, "-": 1, "*": 2, "/": 2}
 print(expression)
 print("input  operators  evaluation")
 for x in expression:
     match x:
-        case "+":                                       #For + and - it's quite redundant since there is no smaller value than 1 in priority
+        case "+":
             operator.append(x)
         case "-":
             operator.append(x)
         case "*":
             if operator:
-                while operator_value[operator[-1]] > operator_value[x]:       #in the * case it checks for smaller values in the operand stack
-                    operator.pop()                                            # and pops them into the evaluation stack
+                while operator_value[operator[-1]] > operator_value[x]:
+                    operator.pop()
             operator.append(x)
         case "/":
             if operator:
-                while operator_value[operator[-1]] > operator_value[x]:        #same for / 
+                while operator_value[operator[-1]] > operator_value[x]:
                     operator.pop()
             operator.append(x)
         case ")":
             operator.append(x)
         case "(":
-            for x in operator[::-1]:
-                while x != ")":
-                    operator.pop()
-                    evaluation.append(x)
+            for y in operator[::-1]:
+                if y == ")":
                     break
+                else:
+                    evaluation.append(y)
+                    operator.pop()
             operator.pop()
         case _:
             evaluation.append(x)
     print(x, "  ", operator, "   ", evaluation)
-for x in operator:
+
+for x in operator[::-1]:
     evaluation.append(x)
     operator.pop()
-    print("    ", operator, "   ", evaluation)
+    print("[]", " ", operator, "   ", evaluation)
+
+print("Final result in polish notation = ","".join(evaluation[::-1]))
+
+
 
